e93d17025ef3e1c123c0e9d6d2e9a2dc080d1632
w4LL4ce-88 <wallaceasilva14@gmail.com>
2024-11-20 23:00:07 -0300

    Funções Básicas
    Inicio do projeto.
    Alterar "caixa" da escrita.
    Tudo maiúscula ou minúscula. Estilos "title" e "capitalize".
    
    Digite o texto na área indicada e apague a '#' que comenta a opção escolhida então execute o script.

---
+++ manipulador_txt.py
@@ -0,0 +1,29 @@
+# CÓDIGO PARA ALTERAÇÕES SIMPLES EM TEXTOS
+
+def primeira_letra(texto):
+    modificado = texto.capitalize()
+    return modificado
+
+def maiuscula(texto):
+    modificado = texto.upper()
+    return modificado
+
+def toda_primeira_letra(texto):
+    modificado = texto.title()
+    return modificado
+
+def minuscula(texto):
+    modificado = texto.lower()
+    return modificado
+
+modificado = 'SEU TEXTO AQUI'  # << Digite seu texto
+
+# ============= ESCOLHER ===============
+
+#print(primeira_letra(modificado))
+
+#print(maiuscula(modificado))
+
+#print(toda_primeira_letra(modificado))
+
+#print(minuscula(modificado))
